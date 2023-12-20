6f2fd30117d140b72564fcfb5f3382e5f78303ca
jitender1222 <jeetusinghkanyal@gmail.com>
2023-12-20 20:54:00 +0530

    added paddle collision logic

--- Javascript/PingPongGame/index.js
+++ Javascript/PingPongGame/index.js
@@ -4,15 +4,13 @@ document.addEventListener("DOMContentLoaded",()=>{
 
     let table=document.getElementById("ping-pong-table");
     let ball=document.getElementById("ball");
+    let paddle=document.getElementById("paddle");
 
     let ballX=50; // distance from the left of the ball w.r.t the ping pong table
     let ballY=50; // distance from the top of the ball w.r.t the ping pong table
 
     let dx=2;  // displacement factor in x-direction ,2 -> you will displace by 2px in +x direction (right side) , -2 -> you wll displace by 2px in -x direction (left side)
     let dy=2   // displacement factor in y-direction ,2 -> you will displace by 2px in +y direction (top side) , -2 -> you will  displace by 2px in -y directipo (down side)
-
-    ball.style.left=`${ballX}px`;
-    // ball.style.top=`${ballY}px`;
 
     setInterval(function exec(){
         ballX+=dx;
@@ -21,8 +19,34 @@ document.addEventListener("DOMContentLoaded",()=>{
         ball.style.left= `${ballX}px` ;
         ball.style.top=  `${ballY}px`;
 
-        if(ballX > table.offsetWidth - ball.offsetWidth || ballX <= 0) dx*=-1;
+        console.log(ballY);
+        if(ballX < paddle.offsetWidth + paddle.offsetLeft && ballY > paddle.offsetTop && ballY - ball.offsetHeight < paddle.offsetTop + paddle.offsetHeight ){
+            dx*=-1;
+        }
+
+        console.log("bally",ballY);
+        console.log("paddle top",paddle.offsetTop)
+
+        if(ballX > table.offsetWidth -  ball.offsetWidth || ballX <= 0) dx*=-1;
         if(ballY > table.offsetHeight - ball.offsetHeight || ballY <= 0) dy*=-1;
     },1)
+
+    let paddleY=0; 
+    let pdY=10;
+
+    document.addEventListener("keydown",(event)=>{
+        event.preventDefault(); // this prevents the execution of the default event behaviour
+        if(event.keyCode==38 && paddleY>0){
+            // up keys 
+            // paddleY+=(-1)*pdY
+             paddleY-=pdY;
+        }
+        else if(event.keyCode==40 && paddleY< table.offsetHeight - paddle.offsetHeight){
+            // downn keys
+            paddleY+=pdY;
+        }
+
+        paddle.style.top=`${paddleY}px`
+    })
 
 });
