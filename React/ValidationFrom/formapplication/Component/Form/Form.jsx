758bac0ea89bf18a00471a36f2df63fb7a81f934
jitender1222 <jeetusinghkanyal@gmail.com>
2024-01-26 16:11:02 +0530

    added useRef

--- React/ValidationFrom/formapplication/Component/Form/Form.jsx
+++ React/ValidationFrom/formapplication/Component/Form/Form.jsx
@@ -1,9 +1,16 @@
-import { useState } from "react"
+import {  useRef, useState } from "react"
 import "./Form.css"
 import emailValidation from "../Validation/emailValidation";
 import passwordValidation from "../Validation/passwordValidation";
 
 const Form=()=>{
+
+    const emailRef=useRef(null);
+    const PassRef=useRef(null);
+    
+    // useEffect(()=>{
+    //     console.log("email",email.current);
+    // })
 
     const [formValue,setFromValue]=useState({
         email:"",
@@ -13,13 +20,15 @@ const Form=()=>{
     const validateEmail=()=>{
         const email=formValue.email;
         if(!emailValidation(email)){
+            emailRef.current.focus();
             console.log("email does not conatin required parameters");
-        }
+        }   
     }
 
     const validatePassword=()=>{
         const password=formValue.password;
         if(!passwordValidation(password)){
+            PassRef.current.focus();
             console.log("password does not conatin required parameters");
         }
     }
@@ -40,9 +49,10 @@ const Form=()=>{
         <h1>Form</h1>
         <form onSubmit={onhandlesubmit}>
         <div className="wrapper input-email-data">
-            <input type="email" 
+            <input type="text" 
             placeholder="Enter Email" 
-            value={formValue.email} 
+            value={formValue.email}
+            ref={emailRef} 
             onChange={(e)=>setFromValue({...formValue,email:e.target.value})}
             />
         </div>
@@ -50,6 +60,7 @@ const Form=()=>{
         <input type="password" 
         placeholder="Enter Password" 
         value={formValue.password}
+        ref={PassRef} 
         onChange={(e)=>setFromValue({...formValue,password:e.target.value})}
         />
         </div>
