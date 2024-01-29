c7b664aa1c10b32ac49d5650df0a4ae110c13a46
jitender1222 <jeetusinghkanyal@gmail.com>
2024-01-29 17:10:07 +0530

    added useRef and useForwardRef

--- React/ValidationFrom/formapplication/Component/Form/Form.jsx
+++ React/ValidationFrom/formapplication/Component/Form/Form.jsx
@@ -1,27 +1,31 @@
 import "./Form.css"
 import Input from "../Input/Input";
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { FormContext } from "../../provider/FormContext";
 
 const Form=()=>{
 
     const {formInput}=useContext(FormContext);
+    const emailRef=useRef(null);
+    const passwordRef=useRef(null);
 
     const onhandlesubmit=(e)=>{
         console.log(formInput);
         e.preventDefault();
+        emailRef.current.focus();
     }
     
     return (
         <>
         <h1>Form</h1>
-        <form onSubmit={onhandlesubmit}>
+        <form onSubmit={onhandlesubmit} noValidate>
         <div className="wrapper input-email-data">
             <Input 
-            type="text"
+            type="email"
             id="email-input" 
             label="email"
             placeholder="Enter Email"
+            ref={emailRef}   // we can't pass it as a ref={emailRef} because it a user component and it is a defined prop for react this is a reserved keyword for react
             />
         </div>
         <div className="wrapper input-password-data">
@@ -29,7 +33,8 @@ const Form=()=>{
         type="password"
         id="password-input"
         label="password"
-        placeholder="Enter Password" 
+        placeholder="Enter Password"
+        inputRef={passwordRef} 
         />
         </div>
 
--- React/ValidationFrom/formapplication/Component/Input/Input.jsx
+++ React/ValidationFrom/formapplication/Component/Input/Input.jsx
@@ -1,22 +1,28 @@
-import { useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import { FormContext } from "../../provider/FormContext";
+import "../Input/input.css"
 
-const Input = ({ type, id, label, placeholder }) => {
+const Input = React.forwardRef(({ type, id, label, placeholder},ref) => {  // if we want to use ref prop then we have to use forwardRef
   const [text, setText] = useState("");
+  const [isValid,setIsValid]=useState(true);
 
   const { formInput, setFormInput } = useContext(FormContext);
+
   return (
     <input
+    className={(!isValid) ? "error-input" : ""}
+    ref={ref}
       type={type}
       placeholder={placeholder}
       id={id}
       value={text}
+
       onChange={(e) => {
         setText(e.target.value);
         setFormInput({ ...formInput, [label]: e.target.value }); // if we want to use a label as a variable from a JS object then we must enclose them inside the [] braces
       }}
     />
   );
-};
+});
 
-export default Input;
+export default Input;  // or we can do React.forwardRef(Input);
