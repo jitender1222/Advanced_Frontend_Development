e3a5d2f4a46da36da6c7804684c758b3e367fde2
jitender1222 <jeetusinghkanyal@gmail.com>
2024-02-05 16:34:46 +0530

    added react forms

--- React/ShoppingCart/shoppinglist/src/Component/Input/Input.jsx
+++ React/ShoppingCart/shoppinglist/src/Component/Input/Input.jsx
@@ -2,29 +2,35 @@ import {showToast} from "../utils/showToast";
 import "react-toastify/dist/ReactToastify.css";
 import "./Input.css"
 import { ToastContainer } from "react-toastify";
-import { useState } from "react";
+import { useForm } from "react-hook-form"
+
 
 const Input=({addItem})=>{
+    const { register, handleSubmit,formState: { errors }, } = useForm();
 
-    const [item,setItem]=useState("");
+    const onSubmit = (data) => {
+        console.log(data);
+        addItem(data.item);
+        showToast("Successfuly Added")
+    }
 
     return (
         <>
+        <form onSubmit={handleSubmit(onSubmit)}>
         <div className="input-items">
-        <input className="input" value={item} placeholder="Enter shopping Items"  onChange={(e)=>setItem(e.target.value)} />
-        <button className="cart-btn"
-        
-        onClick={()=> {
-            addItem(item);
-            setItem(" ");
-            showToast("Successfuly Added")
-            }} >Add To Cart
-        </button>
+        <input className="input" 
+        placeholder="Enter shopping Items"  
+        {...register("item",{ required: true, minLength: 3 })} 
+        />
+
+        <button className="cart-btn">Add To Cart</button>
         <ToastContainer />
         </div>
-       
+        </form>
+        {errors.item && errors.item.type == "required" && <p>Item is Missing</p>}
+        {errors.item && errors.item.type == "minLength" && <p>Item cannot be less than 2</p>}
         </>
-    )
+    )   
 }
 
 export default Input;
---
+++ React/ShoppingCart/shoppinglist/src/Component/providers/context.js
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+
+export const ShoppingListContext=createContext(null);
+export const ShoppingDispatchContext=createContext(null);
